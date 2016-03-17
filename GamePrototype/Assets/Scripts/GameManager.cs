875785500b3cea06f275c9adb4c54181637dd0c5
bergercm <bergercm@umich.edu>
2016-03-17 17:29:41 -0400

    Initial attempts at map creation

---
+++ GamePrototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction {
+	Up, Down, Left, Right
+};
+
+public class GameManager : MonoBehaviour {
+
+	static public GameManager S;
+
+	//main camera
+	public Camera cam;
+
+	//room dimensions
+	private int roomHeight = 12;
+	private int roomWidth = 17;
+
+	//number of players playing the game
+	public int numPlayers = 0;
+
+
+	void Awake(){
+		S = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			MoveCamera (Direction.Up);
+		}
+	}
+
+	public void MoveCamera(Direction dir){
+		Vector3 pos = cam.gameObject.transform.position;
+		print (pos);
+
+		switch (dir) {
+		case Direction.Up:
+			cam.gameObject.transform.position = new Vector3 (pos.x, pos.y + roomHeight, pos.z); 
+			break;
+		case Direction.Down:
+			cam.gameObject.transform.position = new Vector3 (pos.x, pos.y - roomHeight, pos.z); 
+			break;
+		case Direction.Left:
+			cam.gameObject.transform.position = new Vector3 (pos.x - roomWidth, pos.y, pos.z);
+			break;
+		case Direction.Right:
+			cam.gameObject.transform.position = new Vector3 (pos.x + roomWidth, pos.y, pos.z);
+			break;
+		}
+	}
+}
