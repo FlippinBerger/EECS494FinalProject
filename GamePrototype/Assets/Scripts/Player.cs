5531e17b89986c6d4ce9e1e0b3c1130bf1663200
Adam James-Liebau Johnson <riizade@gmail.com>
2016-03-21 00:10:17 -0400

    added mouse input

--- GamePrototype/Assets/Scripts/Player.cs
+++ GamePrototype/Assets/Scripts/Player.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    public float moveSpeed = 5.0f;
-    public GameObject weaponPrefab;
-    public int playerNum = 1;
+    public float moveSpeed = 5.0f; // the movement speed of the player
+    public GameObject weaponPrefab; // the current weapon of the player
+    public int playerNum = 1; // the number of the player
+    public int controllerNum = 0; // the number of the controller used to control this player, 0 indicates mouse + keyboard input
     public float playerRotationAngle = 0f; // the current rotation of the player in degrees
 
     private bool attacking = false; // whether or not the player is currently attacking
+    private bool startAttacking = false; // whether or not the player is starting an attack
     private float attackCooldown; // the total duration of an attack's cooldown (set by the weapon when it attacks)
     private float attackCooldownElapsed = 0.0f; // the time elapsed since the cooldown was initiated
 
@@ -18,24 +20,55 @@ public class Player : MonoBehaviour {
 	}
 
     void Update() {
-        float moveX = Input.GetAxis("P" + playerNum + "LeftHorizontal");
-        float moveY = Input.GetAxis("P" + playerNum + "LeftVertical");
+        if (this.controllerNum > 0) { // if the player is using a controller
+            HandleControllerInput();
+        }
+        else { // if the player is using the mouse and keyboard
+            HandleMouseInput();
+        }
+
+        if (this.startAttacking) { // if the player presses the attack button
+            StartAttack(); // start attacking
+        }
+    }
+
+    void HandleMouseInput() {
+        // get movement input
+        float moveX = Input.GetAxis("MouseLeftHorizontal");
+        float moveY = Input.GetAxis("MouseLeftVertical");
         MovePlayer(moveX, moveY);
 
-        float lookX = Input.GetAxis("P" + playerNum + "RightHorizontal");
-        float lookY = Input.GetAxis("P" + playerNum + "RightVertical");
+        // get look input
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; // get the vector between the mouse and player
+        difference.Normalize(); // normalize the vector
+        float zRotation = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; // get the z-rotation corresponding to the difference vector
+        transform.rotation = Quaternion.Euler(0f, 0f, zRotation - 90); // rotate the player object to look at the mouse
+
+        // get attack input
+        this.startAttacking = Input.GetAxis("MouseFire1") > 0.0f; // set startAttacking if the attack button is pressed
+        if (this.attackCooldownElapsed < this.attackCooldown) { // if attacking is on cooldown
+            this.attackCooldownElapsed += Time.fixedDeltaTime; // update the cooldown time elapsed
+        }
+    }
+
+    void HandleControllerInput() {
+        // get movement input
+        float moveX = Input.GetAxis("P" + controllerNum + "LeftHorizontal");
+        float moveY = Input.GetAxis("P" + controllerNum + "LeftVertical");
+        MovePlayer(moveX, moveY);
+
+        // get look input
+        float lookX = Input.GetAxis("P" + controllerNum + "RightHorizontal");
+        float lookY = Input.GetAxis("P" + controllerNum + "RightVertical");
         if (lookX != 0 || lookY != 0) {
             RotatePlayer(lookX, lookY);
         }
 
-        float attack = Input.GetAxis("P" + playerNum + "Fire1");
+        // get attack input
+        this.startAttacking = Input.GetAxis("P" + controllerNum + "Fire1") < 0.0f; // set startAttacking if the attack button is pressed
         if (this.attackCooldownElapsed < this.attackCooldown) { // if attacking is on cooldown
             this.attackCooldownElapsed += Time.fixedDeltaTime; // update the cooldown time elapsed
         }
-        if (attack != 0) { // if the player presses the attack button
-            StartAttack(); // start attacking
-        }
-
     }
 
     void StartAttack() {
