ee4839a28c53670c7153ab0b0155aa21890abc53
Adam James-Liebau Johnson <riizade@gmail.com>
2016-03-22 13:58:41 -0400

    added enemy AI back in, added aggro distance

--- GamePrototype/Assets/Scripts/Enemy.cs
+++ GamePrototype/Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : Actor {
     public int damage = 1;  // the amount of damage this enemy does to players
     public float knockbackVelocity = 3.0f; // the speed that this enemy knocks players backward
     public float knockbackDuration = 0.1f; // the amount of time this enemy knocks players backward
+    public float aggroDistance; // the distance at which the enemy will start attacking a player
 
     private float targetSelectedTimeElapsed = 0.0f; // the time elapsed since last selecting a target
     private GameObject target; // the target the enemy is trying to move toward
@@ -51,10 +52,10 @@ public class Enemy : Actor {
     }
 
     private void UpdateTarget() {
-        /*
+        
         this.targetSelectedTimeElapsed += Time.deltaTime; // update elapsed time
-
-        if (this.targetSelectedTimeElapsed >= this.targetSelectionInterval) { // if it's time to select a new target
+        
+        //if (this.targetSelectedTimeElapsed >= this.targetSelectionInterval) { // if it's time to select a new target
             this.targetSelectedTimeElapsed = 0.0f; // reset the elapsed time
             // get the location of the closest player
             GameObject closestPlayer = EnemyAIManager.Instance.players[0];
@@ -66,19 +67,22 @@ public class Enemy : Actor {
                     distanceToClosestPlayer = distance;
                 }
             }
-            this.target = closestPlayer; // target the closest player
-        }
-        */
+            if (distanceToClosestPlayer <= this.aggroDistance) { // if a player is within aggro distance
+                this.target = closestPlayer; // target the closest player
+            }
+            else {
+                this.target = this.gameObject; // sit still
+            }
+        //}
+       
     }
 
-    protected override void UpdateMovement() {
-        /*
+    protected override void UpdateMovement() { 
         Vector3 direction = this.target.transform.position - this.transform.position; // determine the direction of the enemy's target
 
         if (!this.knockedBack && !this.recoveringFromHit) { // if the enemy is able to move
             this.transform.position += direction * this.moveSpeed * Time.deltaTime; // move toward the target
         }
-        */
     }
 
     // Update is called once per frame
