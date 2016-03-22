9936f1fe91deacc2782fa84ebe359ddbb0a3164f
MrKibbles <cjlint@umich.edu>
2016-03-22 15:03:24 -0400

    tweaks

--- GamePrototype/Assets/Scripts/WeaponSword.cs
+++ GamePrototype/Assets/Scripts/WeaponSword.cs
@@ -14,7 +14,7 @@ public class WeaponSword : Weapon {
         this.swordRotationAngle = -1 * (this.swingAngle / 2f); // set the starting angle for the sword
         this.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, this.swordRotationAngle)); // update the sword's rotation
         this.parentPlayer = this.transform.parent.gameObject.GetComponent<Player>(); // set the parent player
-        this.transform.localPosition = transform.localRotation * new Vector3(0, 0.8f, 0); // spawn the sword relative to the player
+        this.transform.localPosition = transform.localRotation * new Vector3(0, 1f, 0); // spawn the sword relative to the player
     }
 
     public void OnTriggerEnter2D(Collider2D col) {
@@ -29,7 +29,7 @@ public class WeaponSword : Weapon {
         // update the sword's position
         this.swordRotationAngle += swingSpeed * 100f * Time.deltaTime; // find the sword's new rotation based on swing speed
         this.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, this.swordRotationAngle)); // update the sword's rotation
-        Vector3 pos = new Vector3(0, 0.8f, 0); // get its distance from the center of the player
+        Vector3 pos = new Vector3(0, 1f, 0); // get its distance from the center of the player
         pos = this.transform.localRotation * pos; // rotate the sword around the player
         this.transform.localPosition = pos; // set the sword's position
 
