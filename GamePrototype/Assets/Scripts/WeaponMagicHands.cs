2b6383257a6da764a68886dd9f58485c4d048ff4
MrKibbles <cjlint@umich.edu>
2016-03-29 15:49:00 -0400

    missiles fix

--- GamePrototype/Assets/Scripts/WeaponMagicHands.cs
+++ GamePrototype/Assets/Scripts/WeaponMagicHands.cs
@@ -11,15 +11,17 @@ public class WeaponMagicHands : WeaponRanged {
     public override void Fire(float attackPower) {
         this.numProjectiles = this.minProjectiles + (int)((this.maxProjectiles - this.minProjectiles) * attackPower);
         // calculate middle projectile
+        /*
         int middleProjectile = this.numProjectiles / 2; // initial calculation
         if (this.numProjectiles % 2 != 1) { // if the number of projectiles is odd
             middleProjectile += 1; // add one to the middle projectile calculation
         }
+        */
 
         for (int i = 1; i <= numProjectiles; i++) { // spawn numProjectiles (index starts at 1)
             // determine the rotation for the projectile
-            int index = i - middleProjectile;
-            float angle = index * separationAngle;
+            // int index = i - middleProjectile;
+            float angle = (separationAngle/2) * (numProjectiles - 1) - (separationAngle * (i-1));
             Quaternion newRotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
             GameObject projectileGO = (GameObject)Instantiate(projectilePrefab, transform.position, transform.rotation * newRotation);
