d661ee7de17848f8ac9416173076d8a50d6fe946
Derekiv <derek.eliasen@gmail.com>
2016-04-04 00:41:37 -0400

    Add teleport and blast

--- GamePrototype/Assets/Scripts/WeaponBlast.cs
+++ GamePrototype/Assets/Scripts/WeaponBlast.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WeaponHeal : Weapon {
+public class WeaponBlast : Weapon {
     private float maxSize = 4.0f;
     private float growthRate = 12.0f;
     private float scale = 1.0f;
     private float delayStart = 0.0f;
-    public float delayTime = 2f;
+    public float delayTime = 0.0f;
+    public float knockPower = 1.0f;
+    public float knockDur = 1.0f;
 
     // Use this for initialization
     protected override void Start()
@@ -42,12 +44,11 @@ public class WeaponHeal : Weapon {
     {
         //If an enemy or a projectile hits the shield, knock them back
         //We don't have any projectiles yet, so need to work out what would classify.
-        if (col.gameObject.tag == "Player")
+        if (col.gameObject.tag == "Enemy")
         {
-            Player pp = col.gameObject.GetComponent<Player>();
-            int healthBonus = pp.maxHealth/4;
-            pp.currentHealth += healthBonus;
-            pp.UpdateHealthBar();
+            Vector2 knockbackDirection = col.transform.position - this.parentPlayer.transform.position; // calculate knockback direction
+            knockbackDirection.Normalize(); // make knockbackDirection a unit vector
+            col.gameObject.GetComponent<Enemy>().Knockback(knockPower, knockbackDirection,knockDur); // deal damage to the enemy
         }
     }
 }
---
+++ GamePrototype/Assets/Scripts/WeaponTeleport.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponTeleport : Weapon {
+    protected override void Start()
+    {
+        this.parentPlayer = this.transform.parent.gameObject.GetComponent<Player>(); // set the parent player
+        this.transform.position = this.parentPlayer.transform.position;
+        this.parentPlayer.transform.position += (this.parentPlayer.transform.rotation)  * (Vector3.one * 2.0f);
+        var exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        this.parentPlayer.StopDefense(this.cooldown);
+        Destroy(gameObject, exp.duration);
+    }
+
+    public override void Fire(float attackPower)
+    {
+        hitInfo = DetermineHitStrength(attackPower);
+    }
+}
