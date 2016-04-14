1650e33ff756b43ee0169c37ce751619b17a64dc
bergercm <bergercm@umich.edu>
2016-04-14 14:11:31 -0400

    Forgot a script. All good now

--- GamePrototype/Assets/Scripts/GameManager.cs
+++ GamePrototype/Assets/Scripts/GameManager.cs
@@ -115,8 +115,8 @@ public class GameManager : MonoBehaviour {
 			GameObject p = Instantiate (playerPrefab);
 			Player player = p.GetComponent<Player> ();
 			player.playerNum = i;
-			player.controllerNum = i;
-			// player.controllerNum = 0;
+			//player.controllerNum = i;
+			 player.controllerNum = 0;
 			player.PlacePlayer();
 			players [i - 1] = p;
 		}
