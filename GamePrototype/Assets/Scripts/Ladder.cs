924d0cc7595287a504e672fb00e282437f138be4
MrKibbles <cjlint@umich.edu>
2016-04-14 13:27:01 -0400

    fix ladder bug, moved merchant to correct location, added assetsused.txt

--- GamePrototype/Assets/Scripts/Ladder.cs
+++ GamePrototype/Assets/Scripts/Ladder.cs
@@ -6,21 +6,11 @@ public class Ladder : MonoBehaviour {
 
     int entered = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnTriggerEnter2D(Collider2D other){
-
-        if (Interlocked.Exchange(ref (entered), 1) == 1)
-        {
-            if (other.CompareTag("Player"))
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        { 
+            if (Interlocked.Exchange(ref (entered), 1) == 0)
             {
                 GameManager.S.CleanUpGame();
             }
